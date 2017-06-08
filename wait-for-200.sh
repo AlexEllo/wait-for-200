85055446ea8b0be9932d685d8addb52429958e7c
ello <alex@tradesoft.ru>
2017-06-08 12:37:03 +0300

    Fix call parameters

--- wait-for-200.sh
+++ wait-for-200.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl --retry 100 --retry-delay 3 --retry-max-time 30 --retry-connrefused $1 > /dev/null
+curl --retry 100 --retry-delay 3 --retry-max-time 30 --retry-connrefused $@ > /dev/null
